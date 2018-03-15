487ab6bc3771bf133af2ec1f92b4cb2d40b5cf3b
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-03-15 14:50:39 -0300

    devolviendo index.js a su estado natural xD

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './App.js';
+import App from './App.js';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from './components/navbar';
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="Navbar">
-        <Navbar />
-      </div>
-    );
-  }
-}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
