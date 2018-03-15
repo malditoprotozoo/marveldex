01f83339e3057d544c25f8f5e2ace9829c831ae1
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-03-15 10:46:09 -0300

    añadiendo CharacterContainer a App.js y agregandole eestilos de prueba a CharacterContainer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/navbar';
 import './App.css';
+import CharacterContainer from './components/CharacterContainer/index';
 
 class App extends Component {
   constructor() {
@@ -12,6 +13,7 @@ class App extends Component {
     return (
       <div className="Navbar">
         <Navbar />
+        <CharacterContainer/>
       </div>
     );
   }
--- src/components/CharacterContainer/index.js
+++ src/components/CharacterContainer/index.js
@@ -1,2 +1,12 @@
 import React, { Component } from 'react';
 import Character from './Character/index';
+
+class CharacterContainer extends Component {
+  render() {
+    return (
+      <div style={{width: '100%', backgroundColor: 'red', height: '100vh'}}></div>
+    )
+  }
+}
+
+export default CharacterContainer;
