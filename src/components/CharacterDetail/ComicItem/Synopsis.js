2893faeba89015123e913880dec9f307e9724709
Helvette <malastrugo@live.cl>
2018-03-15 12:02:51 -0400

    organizando characterDetail con reactstrap y upgradeando componentes

---
+++ src/components/CharacterDetail/ComicItem/Synopsis.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Synopsis =( { comicSynopsis } ) => (
+  <span>
+    {comicSynopsis}
+  </span>
+  )
+
+
+export default comicSynopsis;
---
+++ src/components/CharacterDetail/ComicItem/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Title =( { comicTitle } ) => (
+  <span>
+    {comicTitle}
+  </span>
+  )
+
+
+export default Title;
