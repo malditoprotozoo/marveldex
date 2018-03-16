9b33bccc2d17b5412388a18b3bfa50040785586c
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-03-16 01:15:42 -0300

    iterando sobre nuestra lista de personajes importantes

--- src/components/CharacterContainer/Character/index.js
+++ src/components/CharacterContainer/Character/index.js
@@ -5,46 +5,58 @@ import Image from './Image';
 import { Container, Row, Col } from 'reactstrap';
 import './index.css';
 
-const charactersArr = ['Iron Man', 'Captain America', 'Spider-Man'];
-
-class Character extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: '',
-      image: ''
-    }
-  }
-  loadData = () => {
-    fetch('https://gateway.marvel.com:443/v1/public/characters?name=iron%20man&apikey=f2df40d469e17113a48b2711ea0ea74f')
-    .then(response => {
-      return response.json();
-    })
-    .then(response => {
-      this.setState({
-        name: response.data.results[0].name,
-        image: response.data.results[0].thumbnail.path + '.' + response.data.results[0].thumbnail.extension
-      })
-    })
-  }
-  componentDidMount() {
-    this.loadData()
-  }
-  render() {
-    return (
-      <Col xs='12' sm= '6' md='4' lg='2' className='character-item-container'>
-        <div className='character-item'>
-          <Col md='12' className='image'>
-            <Image characterImage = {this.state.image} characterName = {this.state.name} />
-          </Col>
-          <Col md='12' className='name'>
-            <Name characterName = {this.state.name}/>
-          </Col>
-        </div>
-      </Col>
-    )
-  }
+const Character = function({characterName, characterImage}) {
+  return (
+    <Col xs='12' sm= '6' md='4' lg='2' className='character-item-container'>
+      <div className='character-item'>
+        <Col md='12' className='image'>
+          <Image characterImage = {characterImage} characterName = {characterName} />
+        </Col>
+        <Col md='12' className='name'>
+          <Name characterName = {characterName}/>
+        </Col>
+      </div>
+    </Col>
+  )
 }
+// class Character extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       name: '',
+//       image: ''
+//     }
+//   }
+//   loadData = () => {
+//     fetch('https://gateway.marvel.com:443/v1/public/characters?name=iron%20man&apikey=f2df40d469e17113a48b2711ea0ea74f')
+//     .then(response => {
+//       return response.json();
+//     })
+//     .then(response => {
+//       this.setState({
+//         name: response.data.results[0].name,
+//         image: response.data.results[0].thumbnail.path + '.' + response.data.results[0].thumbnail.extension
+//       })
+//     })
+//   }
+//   componentDidMount() {
+//     this.loadData()
+//   }
+//   render() {
+//     return (
+//       <Col xs='12' sm= '6' md='4' lg='2' className='character-item-container'>
+//         <div className='character-item'>
+//           <Col md='12' className='image'>
+//             <Image characterImage = {this.state.image} characterName = {this.state.name} />
+//           </Col>
+//           <Col md='12' className='name'>
+//             <Name characterName = {this.state.name}/>
+//           </Col>
+//         </div>
+//       </Col>
+//     )
+//   }
+// }
 
 
 Container.propTypes = {
--- src/components/CharacterContainer/index.js
+++ src/components/CharacterContainer/index.js
@@ -4,11 +4,49 @@ import Character from './Character/index';
 import { Container, Row, Col } from 'reactstrap';
 import './index.css';
 
+const characters_arr = ['Iron Man', 'Captain America', 'Spider-Man', 'Thor'];
+const char_url = 'https://gateway.marvel.com:443/v1/public/characters';
+const api_key = 'f2df40d469e17113a48b2711ea0ea74f';
+
 class CharacterContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      loading: true
+    };
+    console.log(this.state);
+  }
+  componentWillMount() {
+    let results = [];
+    characters_arr.map(char => {
+      fetch(`${char_url}?name=${char}&apikey=${api_key}`)
+        .then(response => {
+          return response.json();
+        })
+        .then(response => {
+          results.push(response.data.results[0]);
+        })
+        .then(() => {
+          console.log(this.state);
+        })
+        .then(() => {
+          results.length >= characters_arr.length ? this.setState({ data: results, loading: false }) : this.setState({data: results, loading: true});
+        })
+        .catch(error => {
+          console.log('error >' + error);
+        })
+      })
+  }
   render() {
     return (
       <Row className='character-container'>
-        <Character/>
+        {
+        this.state.loading ? <p>insert loader here</p> :
+        this.state.data.map((char, index) =>
+            <Character key={char.name} characterName={char.name} characterImage={char.thumbnail.path + '.' + char.thumbnail.extension}/>
+        )
+        }
       </Row>
     )
   }
