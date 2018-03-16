6be887e862c7e76ea7c54863ae984dcd35ef4d5d
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-03-16 09:52:09 -0300

    llamando a la api en CharacterDetail

--- src/components/CharacterDetail/index.js
+++ src/components/CharacterDetail/index.js
@@ -6,7 +6,41 @@ import CharacterBasic from './CharacterBasic';
 import { Container, Row, Col } from 'reactstrap';
 import './index.css';
 
+const char_url = 'https://gateway.marvel.com:443/v1/public/characters';
+// const api_key = 'f2df40d469e17113a48b2711ea0ea74f';
+// const api_key = 'f3c93215e344b3ad7b6e96ba9ad9faff&hash=b8c91f97165b04fe87a6e262179476d9';
+const api_key = 'df325806086a500e9738f837cba8a7e9&hash=a31a86d05863b60cdc43763bce37f6b5';
+
 class CharacterDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      loading: true 
+    }
+  }
+  componentWillMount() {
+    let str = this.props.location.pathname;
+    let preChar = str.replace('/character/', '');
+    let offChar = preChar.replace('-', ' ');
+    let finalChar = offChar.replace('/', '');
+    fetch(`${char_url}?name=${finalChar}&apikey=${api_key}`)
+      .then(response => {
+        return response.json();
+      })
+      .then(response => {
+        this.setState({
+          data: response.data.results[0],
+          loading: false
+        })
+      })
+      .then(() => {
+        console.log(this.state);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
   render() {
     return (
       <Container>
